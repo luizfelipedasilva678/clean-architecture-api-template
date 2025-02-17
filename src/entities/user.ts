ee25a68b64aa3562e36fa83ba3227acdae200df1
Luiz Felipe <luizfelipedasilva678@gmail.com>
2025-02-16 21:09:40 -0300

    chore: add config to build ts

--- src/entities/user.ts
+++ src/entities/user.ts
@@ -1,25 +1,25 @@
 class User {
-  private readonly id: number;
-  private readonly name: string;
-  private readonly login: string;
+	private readonly id: number;
+	private readonly name: string;
+	private readonly login: string;
 
-  constructor(id = 0, name = "", login = "") {
-    this.id = id;
-    this.name = name;
-    this.login = login;
-  }
+	constructor(id = 0, name = "", login = "") {
+		this.id = id;
+		this.name = name;
+		this.login = login;
+	}
 
-  public getId(): number {
-    return this.id;
-  }
+	public getId(): number {
+		return this.id;
+	}
 
-  public getName(): string {
-    return this.name;
-  }
+	public getName(): string {
+		return this.name;
+	}
 
-  public getLogin(): string {
-    return this.login;
-  }
+	public getLogin(): string {
+		return this.login;
+	}
 }
 
 export default User;
